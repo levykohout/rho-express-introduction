f4da1425ee9ef84e8153d319bbd8740883cf4bc6
Ryan Mulcahy <ryan@primeacademy.io>
2016-10-05 12:00:36 -0500

    Code from lecture

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+// middleware function, gets executed on each request
+app.use(function(req, res, next){
+  console.log('Got a request!');
+  next();
+});
+
+// middleware for parsing the body and turning it into a JS object
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/', function(req, res){
+  console.log('req.body=', req.body);
+  res.sendStatus(200);
+});
+
+app.get('/', function(req, res){
+  console.log('Received a request at', new Date());
+  // __dirname is the folder this file lives in
+  var filename = path.join(__dirname, 'public/views/index.html');
+  console.log('filename:', filename);
+  res.sendFile(filename);
+});
+
+app.get('/kittens', function(req, res){
+  console.log('Query params:', req.query);
+  if (req.query.age > 2) {
+    res.send('MEOW!');
+  } else {
+    res.send('meow');
+  }
+});
+
+// middleware for serving static files
+app.use(express.static('public'));
+
+app.listen(3000);
